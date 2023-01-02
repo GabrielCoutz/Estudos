05e88800e7a713dbfd0f94c355a0db12e9ad2d6c
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-02 15:59:22 -0300

    filters

--- Vuejs/js/script.js
+++ Vuejs/js/script.js
@@ -1,13 +1,28 @@
 const options = {
   el: "#app",
   data: {
-    number: 0,
+    products: [],
+    product: {},
   },
-  methods: {},
-  watch: {
-    number() {
-      document.title = this.number;
+  methods: {
+    async fetchProducts() {
+      this.products = await (await fetch("./api/produtos.json")).json();
     },
+
+    async fetchProduct(id) {
+      this.product = await (await fetch(`./api/${id}`)).json();
+    },
+  },
+  filters: {
+    numberToCurrency(number) {
+      return number.toLocaleString("BRL", {
+        currency: "BRL",
+        style: "currency",
+      });
+    },
+  },
+  created() {
+    this.fetchProducts();
   },
 };
 
