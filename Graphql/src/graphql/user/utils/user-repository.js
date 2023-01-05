fde1aca9080b807bbb3a25bf3ef5495482fd662b
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-05 09:43:05 -0300

    PasswordHash

--- Graphql/src/graphql/user/utils/user-repository.js
+++ Graphql/src/graphql/user/utils/user-repository.js
@@ -1,16 +1,29 @@
-import { ValidationError } from "apollo-server";
+import { UserInputError, ValidationError } from "apollo-server";
+import bcrypt from "bcrypt";
 
 export async function createUserFn(data, dataSource) {
   const userInfo = await createUserInfo(data, dataSource);
-
   return await dataSource.post("", { ...userInfo });
+}
+
+function validateUserPassword(password) {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,30}$/;
+  if (!password.match(regex))
+    throw new UserInputError(
+      "Password must contain at least: One lower case letter, One uppercase letter and one number"
+    );
 }
 
 async function createUserInfo(data, dataSource) {
   const { firstName, lastName, password } = data;
+  let passwordHash;
 
   if (!firstName || !lastName || !password)
-    throw new ValidationError("You have to send firstName and lastName.");
+    throw new ValidationError(
+      "You have to send firstName, lastName and password."
+    );
+
+  validateUserPassword(password);
 
   const indexRefUser = await dataSource.get("", {
     _limit: 1,
@@ -19,12 +32,18 @@ async function createUserInfo(data, dataSource) {
   });
   const indexRef = +indexRefUser[0].indexRef + 1;
 
+  if (password && !data.passwordHash) {
+    passwordHash = await bcrypt.hash(password, 12);
+    delete data["password"];
+  }
+
   return {
     firstName,
     lastName,
     userName: `${firstName.toLowerCase()}.${lastName.toLowerCase()}`,
     indexRef,
     createdAt: new Date().toISOString(),
+    passwordHash,
   };
 }
 
