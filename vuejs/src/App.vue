a47fc6b9b6f2b9fd8ca55a6a79028d093721ba39
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-06 17:25:18 -0300

    Animation modal

--- vuejs/src/App.vue
+++ vuejs/src/App.vue
@@ -1,34 +1,87 @@
 <template>
   <div id="app">
-    <keep-alive>
-      <component :is="activeComponent"></component>
-    </keep-alive>
-    <select v-model="activeComponent">
-      <option value="" selected disabled>Selecione o componente</option>
-      <option value="Sobre">Sobre</option>
-      <option value="Servicos">Servicos</option>
-      <option value="Formulario">Formulario</option>
-    </select>
+    <button @click="modalActive = !modalActive">Ativar</button>
+    <transition name="fade">
+      <div class="modal-container" v-if="modalActive" @click="outsideClick">
+        <div class="modal">
+          <button @click="modalActive = !modalActive">X</button>
+          <h1>modal</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolor
+            sed laudantium inventore
+          </p>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <script>
-import Sobre from "./components/Sobre.vue";
-import Servicos from "./components/Servicos.vue";
-import Formulario from "./components/Formulario.vue";
-
 export default {
   name: "App",
-  components: {
-    Sobre,
-    Servicos,
-    Formulario,
-  },
   data() {
     return {
-      activeComponent: "",
+      modalActive: true,
     };
+  },
+  methods: {
+    outsideClick({ target, currentTarget }) {
+      if (target === currentTarget) this.modalActive = !this.modalActive;
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.modal {
+  background: #123;
+  color: white;
+  padding: 3rem;
+  border-radius: 24px;
+  position: relative;
+}
+
+.modal button {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  border: none;
+  cursor: pointer;
+  padding: 0.3rem;
+  font-size: 1rem;
+  color: black;
+  background: white;
+  border-radius: 50%;
+  width: 44px;
+  height: 44px;
+}
+
+.modal p {
+  max-width: 40ch;
+}
+
+.modal-container {
+  display: grid;
+  align-content: center;
+  justify-items: center;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.4);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
