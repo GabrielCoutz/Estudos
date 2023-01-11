9ecf003ed84a94b9114f603a61618f1ccc30046f
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-11 10:12:28 -0300

    Categorias pronta

--- praticalandingpage/components/Categorias.tsx
+++ praticalandingpage/components/Categorias.tsx
@@ -8,10 +8,18 @@ const Categorias = () => {
         <h1>Categories</h1>
         <p>See all</p>
       </div>
-      <ul>
+      <ul className={styles.cards}>
         <li className={styles.card}>
-          <div></div>
+          <div className={styles.image}></div>
           <span>Comedy</span>
+        </li>
+        <li className={styles.card}>
+          <div className={styles.image}></div>
+          <span>Romantic</span>
+        </li>
+        <li className={styles.card}>
+          <div className={styles.image}></div>
+          <span>Fantasy</span>
         </li>
       </ul>
     </section>
