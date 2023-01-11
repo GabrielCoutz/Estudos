96a9a69de022e44d20ffed7fc7450a84026e5975
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-01-10 22:00:16 -0300

    Intro pronta

--- praticalandingpage/components/Intro.tsx
+++ praticalandingpage/components/Intro.tsx
@@ -4,17 +4,32 @@ import styles from "../styles/Intro.module.scss";
 
 const Intro = () => {
   return (
-    <main className={`${styles.intro} container`}>
-      <h1>
-        A book is a <br />
-        portrait of truth
-      </h1>
-      <div className={styles.searchBox}>
-        <Search />
-        <input type="text" placeholder="Looking for a book" />
-        <button>{">"}</button>
+    <>
+      <main className={`${styles.intro} container`}>
+        <h1>
+          A book is a <br />
+          portrait of truth
+        </h1>
+        <div className={styles.searchBox}>
+          <Search />
+          <input type="text" placeholder="Looking for a book" />
+          <button>{">"}</button>
+        </div>
+      </main>
+      <div className={styles.faixaWrapper}>
+        <div className={`${styles.faixa}`}>
+          <div className={styles.faixaBg}></div>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+          <span>New Books</span>
+        </div>
       </div>
-    </main>
+    </>
   );
 };
 
