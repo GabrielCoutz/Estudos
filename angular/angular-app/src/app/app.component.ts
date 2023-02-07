2eea7ed44965767bc191ad32914af538f392de49
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-07 09:20:13 -0300

    Input & Output

--- angular/angular-app/src/app/app.component.ts
+++ angular/angular-app/src/app/app.component.ts
@@ -4,4 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: 'app.template.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  addValue: number = 0;
+  eventData: { name: string; age: number } | undefined;
+
+  add() {
+    this.addValue++;
+  }
+
+  getData(event: { name: string; age: number }) {
+    this.eventData = event;
+  }
+}
--- angular/angular-app/src/app/shared/shared.module.ts
+++ angular/angular-app/src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewComponentComponent } from './new-component/new-component.component';
+import { InputComponent } from './input/input.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
-  declarations: [NewComponentComponent],
+  declarations: [NewComponentComponent, InputComponent, OutputComponent],
   imports: [CommonModule],
-  exports: [NewComponentComponent],
+  exports: [NewComponentComponent, InputComponent, OutputComponent],
 })
 export class SharedModule {}
