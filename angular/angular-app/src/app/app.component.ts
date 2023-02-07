4633b02a2345fc33c67b7b2b02ccacaf462f58d0
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-07 09:35:17 -0300

    App todo

--- angular/angular-app/src/app/app.component.ts
+++ angular/angular-app/src/app/app.component.ts
@@ -5,14 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.template.html',
 })
 export class AppComponent {
-  addValue: number = 0;
-  eventData: { name: string; age: number } | undefined;
 
-  add() {
-    this.addValue++;
-  }
-
-  getData(event: { name: string; age: number }) {
-    this.eventData = event;
-  }
 }
--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -1,35 +1,14 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TitleComponent } from './title/title.component';
-import { DataBidingComponent } from './data-biding/data-biding.component';
 import { FormsModule } from '@angular/forms';
-import { DiretivasComponent } from './diretivas/diretivas.component';
-import { AtributosComponent } from './atributos/atributos.component';
-
-import ptBr from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common';
-import { SharedModule } from './shared/shared.module';
-
-registerLocaleData(ptBr);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TitleComponent,
-    DataBidingComponent,
-    DiretivasComponent,
-    AtributosComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'pt-BR',
-    },
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
