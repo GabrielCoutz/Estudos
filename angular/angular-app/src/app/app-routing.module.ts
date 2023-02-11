96e74556e3afd315ccd2d84ed23befe9212f0b3c
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-11 19:47:34 -0300

    Projeto pokedex - inicio

--- angular/angular-app/src/app/app-routing.module.ts
+++ angular/angular-app/src/app/app-routing.module.ts
@@ -1,38 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { SobreComponent } from './pages/sobre/sobre.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full',
-  },
-  {
-    path: 'sobre',
-    component: SobreComponent,
-    children: [
-      {
-        path: ':id',
-        component: SobreComponent,
-      },
-    ],
-  },
-  {
-    path: 'dashboard',
-    loadChildren: async () =>
-      (await import('./dashboard/dashboard.module')).DashboardModule,
-  },
-  {
-    path: '404',
-    component: NotFoundComponent,
-  },
-  {
-    path: '**',
-    redirectTo: '404',
-  },
 ];
 
 @NgModule({
--- angular/angular-app/src/app/app.module.ts
+++ angular/angular-app/src/app/app.module.ts
@@ -3,15 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HomeComponent } from './pages/home/home.component';
-import { SobreComponent } from './pages/sobre/sobre.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SobreComponent, NotFoundComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
