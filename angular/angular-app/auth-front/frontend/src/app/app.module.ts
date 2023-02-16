47cea403cc963c413d9d4d1d7233cbb901875559
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-16 15:47:37 -0300

    feat(/auth-front): inicio do projeto de autenticação p/login com rotas

---
+++ angular/angular-app/auth-front/frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/admin/pages/home/home.component';
+import { AuthComponent } from './core/components/pages/auth/auth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AuthComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
