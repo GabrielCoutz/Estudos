f3c32819dd6cc7dc052160e6a439a933704c1a92
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-17 11:20:01 -0300

    feat(signin.component): componente de formulário já pronto e enviando dados para o método 'submit'

--- angular/angular-app/auth-front/frontend/src/app/core/components/auth/auth.module.ts
+++ angular/angular-app/auth-front/frontend/src/app/core/components/auth/auth.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SiginComponent } from './pages/sigin/sigin.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    SiginComponent
-  ],
+  declarations: [SiginComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
--- angular/angular-app/auth-front/frontend/src/app/core/components/auth/pages/sigin/sigin.component.ts
+++ angular/angular-app/auth-front/frontend/src/app/core/components/auth/pages/sigin/sigin.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sigin',
   templateUrl: './sigin.component.html',
-  styleUrls: ['./sigin.component.scss']
+  styleUrls: ['./sigin.component.scss'],
 })
 export class SiginComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  public formAuth: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  submit(): void {
+    const formIsValid = this.formAuth.valid;
+
+    if (!formIsValid) return;
+
+    console.log(this.formAuth.value);
+  }
 }
