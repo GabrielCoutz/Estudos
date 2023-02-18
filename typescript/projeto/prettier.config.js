7b0b31b0a035fae7ec33e41af4f7a1bcef754990
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-18 19:03:56 -0300

    feat(/projeto): inicio do projeto do curso de typescript & angular
    Este projeto que usa Angular e Typescript, está sendo construído com o eslint e prettier instalados
    via NPM, e não mais em extensões do VScode. Para fins de aprendizado e visualização do
    comportamento.

---
+++ typescript/projeto/prettier.config.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+	singleQuote: true,
+	traillingComa: 'all',
+	arrowParams: 'avoid',
+	tabWidth: 2,
+	useTabs: true,
+	minItems: 0,
+	arrayExpand: false,
+};
