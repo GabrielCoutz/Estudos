8f431f223442b7670631b1419eb96da8ae3a922a
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-23 17:32:16 -0300

    feat: parte visual pronta para receber lógica com js

--- angular-e-typescript/projeto/src/app/shared/painel/painel.component.spec.ts
+++ angular-e-typescript/projeto/src/app/shared/painel/painel.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PainelComponent } from './painel.component';
 
 describe('PainelComponent', () => {
-  let component: PainelComponent;
-  let fixture: ComponentFixture<PainelComponent>;
+	let component: PainelComponent;
+	let fixture: ComponentFixture<PainelComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PainelComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [PainelComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(PainelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(PainelComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
