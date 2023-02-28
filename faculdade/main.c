3533de4b9a4f93e853cbfe8128d650d63a62e432
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-28 14:09:33 -0300

    feat(main.c): switch em C

--- faculdade/main.c
+++ faculdade/main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-void main () {
-  printf("xampson\n");
+
+int main () {
+  int numero;
+  printf("\nInsira um numero: ");
+  scanf("%d", &numero);
+  switch (numero % 2)
+  {
+  case 0:
+    printf("O numero %d e par", numero);
+    break;
+  
+  default:
+  printf("O numero %d e impar", numero);
+    break;
+  }
+  return 0;
 }
