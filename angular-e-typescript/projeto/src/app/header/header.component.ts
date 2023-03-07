6357f480ca0f8e44122933707a4a9c61d0952fad
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-03-07 14:13:42 -0300

    feat(headercomponent.ts): input de pesquisa implementado e valor já sendo recebido pelo componente

--- angular-e-typescript/projeto/src/app/header/header.component.ts
+++ angular-e-typescript/projeto/src/app/header/header.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	pesquisa(value: string) {
+		console.log(value);
+	}
+}
